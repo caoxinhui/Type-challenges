b25de98706df46c843ba8d530881e3585ce22c86
caoxinhui <caoxinhui@baidu.com>
2022-06-18 11:12:27 +0800

    feat: add

---
+++ 00018-easy-tuple-length.ts
@@ -0,0 +1,14 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+]
+
+
+// ============= Your Code Here =============
+type Length<T extends readonly any[]> = T['length']
